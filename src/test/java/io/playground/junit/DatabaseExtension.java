1c98a273caf983cf78d3d9a8906a18b21ca9e615
aymens <sayhi.aymen@gmail.com>
2025-01-17 18:02:42 +0100

    fix: add forgotten test spring.datasource.* props update upon container startup

--- src/test/java/io/playground/junit/DatabaseExtension.java
+++ src/test/java/io/playground/junit/DatabaseExtension.java
@@ -18,15 +18,9 @@ public class DatabaseExtension implements BeforeAllCallback {
     public void beforeAll(ExtensionContext context) throws Exception {
         if (!POSTGRES.isRunning()) {
             log.info("Starting database ...");
-            POSTGRES.start(); // don't rely on @Container anymore as its prevent reuse by killing the container when test class finishes
+            POSTGRES.start(); // don't rely on @Container anymore as it prevents containers reuse by killing them when test class finishes
         } else {
             log.info("Database is already up.");
-        }
-
-        if (!shutdownHookAdded) {
-            Runtime.getRuntime().addShutdownHook(new Thread(POSTGRES::stop));
-            shutdownHookAdded = true;
-            log.info("Database shutdown hook set.");
         }
 
         log.info("Database infos: jdbc:postgresql://{}:{}/{}; user/pwd: {}/{}",
@@ -35,5 +29,17 @@ public class DatabaseExtension implements BeforeAllCallback {
                 POSTGRES.getDatabaseName(),
                 POSTGRES.getUsername(),
                 POSTGRES.getPassword());
+
+        System.setProperty("spring.datasource.url", POSTGRES.getJdbcUrl());
+        System.setProperty("spring.datasource.username", POSTGRES.getUsername());
+        System.setProperty("spring.datasource.password", POSTGRES.getPassword());
+
+        log.info("Updated properties 'spring.datasource.*'.");
+
+        if (!shutdownHookAdded) {
+            Runtime.getRuntime().addShutdownHook(new Thread(POSTGRES::stop));
+            shutdownHookAdded = true;
+            log.info("Database shutdown hook set.");
+        }
     }
 }
